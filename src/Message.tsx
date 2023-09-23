04b85b13d7c3b002d557267fd2316bdbaa18ff92
Sebastian <sebastiansloma@gmail.com>
2023-09-23 10:30:51 +0200

    add type

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,4 +1,4 @@
-export const Message = ({author, children}) =>{
+export const Message = ({author, children}: {author: string, children: string}) =>{
     return (
         <p>
             <b>{author}</b>: {children}
