b8bf653fc097060d21987ec6be2352e57ccd7d10
Sebastian <sebastiansloma@gmail.com>
2023-09-23 10:29:10 +0200

    add message

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import { Message } from './Message'
 
 function App() {
   
@@ -7,6 +8,7 @@ function App() {
   return (
    <div>
     <h1>Salut</h1>
+    <Message author='Merlin'>TypeScript</Message>
    </div>
   )
 }
