73e88ac8ea1887747d55e961c76590c75c6601f4
Sebastian <sebastiansloma@gmail.com>
2023-09-24 11:46:54 +0200

    added variant

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
    <div>
     <h1>Salut</h1>
     <Message author='Merlin'>TypeScript</Message>
-    <Message author='Julio'><span style={ color:"red" }>JavaScript</span></Message>
+    <Message author='Julio' variant='LEFT'><span style={ {color:"red"} }>JavaScript</span></Message>
    </div>
   )
 }
--- src/Message.tsx
+++ src/Message.tsx
@@ -1,13 +1,28 @@
 import { PropsWithChildren } from 'react';
 
+type MessageVariant = 'RIGHT' | 'LEFT';
+
 type MessageProps = PropsWithChildren<{
 	author: string;
+	variant?: MessageVariant;
 }>;
 
-export const Message = ({ author, children }: MessageProps) => {
+export const Message = ({
+	author,
+	children,
+	variant = 'RIGHT',
+}: MessageProps) => {
 	return (
-		<p>
-			<b>{author}</b>: {children}
+		<p
+			style={{
+				display : 'flex',
+				flexDirection: variant === 'RIGHT' ? 'row' : 'row-reverse',
+			}}
+		>
+			<b>
+				{author}
+			</b>
+			{''}: {children}
 		</p>
 	);
 };
